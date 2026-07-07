e218615d9007eaba2827e2fc7aadf8d97c0262b6
M1lachite <99134679+M1lachite@users.noreply.github.com>
2026-07-07 13:08:16 +0200

    Add public subnets and routing for VPC
    feat(network): finish subnets, add gateway and route table

--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -2,22 +2,59 @@ resource "aws_vpc" "main_vpc" {
   cidr_block = var.vpc_cidr
 }
 
-resource "aws_subnet" "eks_subnet_1" {
+resource "aws_subnet" "public_1" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "eks-subnet-1"
+    Name = "public-subnet-1"
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
 }
 
-resource "aws_subnet" "eks_subnet_2" {
+resource "aws_subnet" "public_2" {
   vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = "10.0.32.0/24"
   availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "eks-subnet-2"
+    Name = "public-subnet-2"
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "route-table"
+  }
+}
+
+resource "aws_route_table_association" "route_table_association_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "route_table_association_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.route_table.id
 }
