4ce919c2cff99596ab1b6ff18395404f76314288
M1lachite <Kemik94@gmail.com>
2026-07-13 21:28:56 +0200

    feat(iam): create first role / policy attachment.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,17 +1,17 @@
-provider "aws" {  
+provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region = "us-east-1"
+  region     = "us-east-1"
   endpoints {
-    eks = var.destination
-    ec2 = var.destination
-    iam = var.destination
-    sts = var.destination
-    ecr = var.destination
-    s3 = var.destination
+    eks      = var.destination
+    ec2      = var.destination
+    iam      = var.destination
+    sts      = var.destination
+    ecr      = var.destination
+    s3       = var.destination
     dynamodb = var.destination
-    rds = var.destination
-    sqs = var.destination
-    sns = var.destination
+    rds      = var.destination
+    sqs      = var.destination
+    sns      = var.destination
   }
 }
