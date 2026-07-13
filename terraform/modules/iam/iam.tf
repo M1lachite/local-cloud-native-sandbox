a08d89b4a66ea1372f2b2cb81c90b1e33eb06e40
M1lachite <Kemik94@gmail.com>
2026-07-13 21:45:31 +0200

    feat(iam): Second role and policies for it.

--- terraform/modules/iam/iam.tf
+++ terraform/modules/iam/iam.tf
@@ -15,9 +15,37 @@ resource "aws_iam_role" "eks_control_plane_role" {
     Name = "eks-control-plane-role"
   }
 }
-
+resource "aws_iam_role" "eks_node_group_role" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+  tags = {
+    Name = "eks-node-group-role"
+  }
+}
 resource "aws_iam_role_policy_attachment" "eks_control_plane_role_policy_attachment" {
   role       = aws_iam_role.eks_control_plane_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 
+}
+resource "aws_iam_role_policy_attachment" "eks_node_group_role_policy_attachment" {
+  role       = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+resource "aws_iam_role_policy_attachment" "eks_node_group_role_policy_attachment_2" {
+  role       = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+resource "aws_iam_role_policy_attachment" "eks_node_group_role_policy_attachment_3" {
+  role       = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
