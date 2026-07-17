f3d2bd25b3641c7eca920366cec9764185f93e34
M1lachite <Kemik94@gmail.com>
2026-07-17 19:59:03 +0200

    feat(eks): quick variables.tf improvement

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -1,13 +1,7 @@
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "test_floci-eks-cluster"
+  default     = "floci-eks-cluster"
 }
 
 
